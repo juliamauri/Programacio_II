3b024bf99a382faa00e03201209250da0fcd9081
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-02-17 09:18:22 +0100

    Testing with if

---
+++ 0. Proves/0.1. C/if_else if_else/if_else if_else/Nombre.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int a = 5;
+
+	if (a < 0 ){}
+	else if(a = 0){}
+	else{ printf(":3") }
+	/*else if(a > 5){printf("Hello")}
+	else{}*/
+	
+
+	
+	return 0;
+}
