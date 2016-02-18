e8d0d1a30217fac98dccdb272c456939383d8625
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-02-18 22:21:05 +0100

    Added the second exercise

---
+++ 2. Some Exercises - Flow Control/2/2/Main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int num = 0, result = 0;
+
+	printf("Tell me 10 numbers, then I tell you which number is the smallest:\n");
+
+	for(int i = 0; i < 10; i++){
+
+		printf("Tell me de %i number: ", i + 1);
+		scanf_s("%i", &num);
+		if ( i == 0){
+			result = num;
+		}
+		else if (result > num){
+			result = num;
+		}
+	}
+
+	printf("The smallest number is: %i\n", result);
+	
+
+
+	system("pause");
+	return 0;
+}
