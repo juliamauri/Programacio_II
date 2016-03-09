437413bbb5673fadf7789a19c8a07b2d4c6759df
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-03-09 02:54:35 +0100

    Updated

--- 6. Introduction to C++/Introduction/Introduction/Main.cpp
+++ 6. Introduction to C++/Introduction/Introduction/Main.cpp
@@ -7,26 +7,36 @@ struct Point
 		x = 0;
 		y = 0;
 	}
+	
+	void SetPoint(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+	
+	void SetPoint(Point data_referent)
+	{
+		x = data_referent.x;
+		y = data_referent.y;
+	}
+
+	bool isPointZero()
+	{
+		return x == 0 && y == 0;
+	}
+
+	void DistanceTo(Point data_referent)
+	{
+		x = data_referent.x - x;
+		y = data_referent.y - y;
+	}
 };
 
 
 
-void SetPoint(Point* data_to_modify, int x, int y)
-{
-	data_to_modify->x = x;
-	data_to_modify->y = y;
-}
 
-void SetPoint(Point* data_to_modify, Point data_referent)
-{
-	data_to_modify->x = data_referent.x;
-	data_to_modify->y = data_referent.y;
-}
 
-bool isPointZero(Point my_point)
-{
-	return my_point.x == 0 && my_point.y == 0;
-}
+
 
 int main()
 {
@@ -34,17 +44,20 @@ int main()
 	Point p;
 	Point b;
 
-	p.SetZero; // SetPointZero(&p)
-	b.SetZero;
+	p.SetZero(); // SetPointZero(&p)
+	b.SetZero();
 
-	bool result_a = isPointZero(p);
+	bool result_a = p.isPointZero();
 
-	SetPoint(&p, 3, 4);
+	p.SetPoint(3, 4);
 
-	bool result_b = isPointZero(p);
+	bool result_b = p.isPointZero();
 
-	SetPoint(&p, b);
+	p.SetPoint(b);
 
+	p.SetPoint(3, 4);
+
+	b.DistanceTo(p);
 
 	return 0;
 }
