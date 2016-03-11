6a24b6f39f7b59870a86841d3f61bd9f55688247
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-03-11 12:08:01 +0100

    Added Herency, etc

--- 6. Introduction to C++/Introduction/Introduction/Main.cpp
+++ 6. Introduction to C++/Introduction/Introduction/Main.cpp
@@ -1,11 +1,21 @@
 #include "Point.h"
-
+#include <stdio.h>
 
 int main()
 {
-	
+	/*
 	Point p;
 	Point b;
+
+	Point *a;
+
+	Point* c;
+
+	Point* p2 = new Point;
+
+	Point* p3 = new Point[10];
+
+	Point** p4 = new Point*[10];
 
 	{
 		Point x;
@@ -25,8 +35,20 @@ int main()
 	p.SetPoint(3, 4);
 
 	b.DistanceTo(p);
-
+	*/
 	//----------------------------------------------------
+	printf("\n\n");
+	//----------------------------------------------------
+
+	Bicho* horda[100];
+	
+	for (int i = 0; i < 30; i++) Bicho* b2 = new Troll(); 
+
+	for (int i = 30; i < 100; i++) Bicho* b1 = new goblin();
+	
+	for (int i = 0; i < 100; i++) horda[i]->attack();
+
+
 
 
 
