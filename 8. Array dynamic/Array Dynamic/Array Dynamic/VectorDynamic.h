29ff89aa2c178bb83e1901d8476d3778594d72f8
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-04-13 10:02:37 +0200

    UPDATED: Array dynamic

--- 8. Array dynamic/Array Dynamic/Array Dynamic/VectorDynamic.h
+++ 8. Array dynamic/Array Dynamic/Array Dynamic/VectorDynamic.h
@@ -7,6 +7,7 @@ private:
 	TYPE* buffer;
 	unsigned int capacity = 10;
 	unsigned int num_elements = 0;
+	
 
 public:
 	Vector()
@@ -27,7 +28,89 @@ public:
 	{
 		delete[] buffer;
 	}
+
 	//fer metode push_back(principi) i push_front(al principi)
+	void push_back(const TYPE &element){
+		
+		if (capacity == num_elements)
+		{
+			TYPE *temp = nullptr;
+			capacity *= 2;
+			temp = new TYPE[capacity];
+
+			for (unsigned int i = 0; i < num_elements; i++)
+			{
+				temp[i] = buffer[i];
+			}
+			delete[] buffer;
+			buffer = temp;
+		}
+
+		buffer[num_elements++] = element;
+	}
+
+	void push_front(const TYPE &element){
+		if (capacity == num_elements)
+		{
+			TYPE *temp = nullptr;
+			capacity *= 2;
+			temp = new TYPE[capacity];
+
+			for (unsigned int i = 0; i < num_elements; i++)
+			{
+				temp[i] = buffer[i];
+			}
+			delete[] buffer;
+			buffer = temp;
+		}
+
+		num_elements++;
+		for (unsigned int i = 1; i < num_elements; i++)
+			buffer[i] = buffer[i-1];
+		buffer[0] = element;
+	}
+
+	void PaintVector()const
+	{
+		for (unsigned int i = 0; i < num_elements; i++)
+			printf("%i ", buffer[i]);
+		printf("\n");
+	}
+
+	bool empty(){
+		return num_elements == 0 ? 1 : 0;
+	}
+
+	void clean(){
+		delete[] buffer;
+		buffer = new TYPE[capacity];
+	}
+
+	int size(){
+		return num_elements;
+	}
+
+	int capacityn(){
+		return capacity;
+	}
+
+	void pop_back(){
+		buffer[num_elements--] = 0;
+	}
+
+	void shrink_to_fit(){
+		TYPE* temp = nullptr;
+		capacity = num_elements;
+		temp = new TYPE[capacity];
+
+		for (unsigned int i = 0; i < num_elements; i++)
+			temp[i] = buffer[i];
+
+		delete[] buffer;
+
+		buffer = temp;
+	}
+
 };
 
 #endif
--- 8. Array dynamic/Array Dynamic/Array Dynamic/main.cpp
+++ 8. Array dynamic/Array Dynamic/Array Dynamic/main.cpp
@@ -1,11 +1,50 @@
 #include "VectorDynamic.h"
+#include <stdio.h>
 
 int main(){
 	Vector<int> v;
-	Vector<char> s;
-	Vector<float> f;
+	Vector<int> c;
 
-	Vector<int> cv(v);
+	v.push_back(9);
+	v.PaintVector();
+	v.push_back(5);
+
+	v.PaintVector();
+
+	v.push_front(2);
+	v.PaintVector();
+	v.push_front(3);
+
+	v.PaintVector();
+
+
+	bool jej = c.empty();
+
+	c.push_back(5);
+
+	bool jej2 = c.empty();
+
+	c.clean();
+
+	bool jej3 = c.empty();
+
+	int size = v.size();
+
+	int capacity = v.capacityn();
+
+	v.pop_back();
+
+	v.PaintVector();
+
+	v.shrink_to_fit();
+	
+	int capacity2 = v.capacityn();
+
+	v.push_back(5);
+
+	v.PaintVector();
+	
+	int capacity3 = v.capacityn();
 
 	return 0;
 }
